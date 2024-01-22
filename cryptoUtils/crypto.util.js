b5688296c9a68a63661b16ef46db26a9f1f19bad
Pablo Martin Tedesco Rordiguez <tedesco8@gmail.com>
2024-01-22 15:15:23 -0300

    Encryption method is changed to the crypto api.

--- cryptoUtils/crypto.util.js
+++ cryptoUtils/crypto.util.js
@@ -1,17 +1,21 @@
-import {JSEncrypt } from 'jsencrypt';
-
 export const decodeToken = (token) => {
     let base64Url = token.split('.')[1];
     let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     let jsonPayload = decodeURIComponent(atob(base64).split('').map(c => {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
     }).join(''));
-    
+
     return JSON.parse(jsonPayload);
 }
 
-export const encryptStr = (publicKey, str) => {
-    const encrypt = new JSEncrypt();
-    encrypt.setPublicKey(publicKey);
-    return encrypt.encrypt(str);
+export const encrypt = (publicKey, data, algorith = "RSA-OAEP") => {
+    let encoded = new TextEncoder();
+    encoded.encode(data)
+    return window.crypto.subtle.encrypt(
+        {
+            name: algorith,
+        },
+        publicKey,
+        encoded,
+    );
 }
