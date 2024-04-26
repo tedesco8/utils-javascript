00bd0209222ae9c96c916667ace7ce0ca07150ce
Pablo Tedesco <tedesco8@gmail.com>
2024-04-26 17:35:19 -0300

    [ArrayUtils] - Added recursive binary search algorithm

--- arrayUtils/array.util.js
+++ arrayUtils/array.util.js
@@ -8,3 +8,17 @@ export const getRandomItem = (array) => {
 }
 
 export const removeDuplicateItem = (array) => [...new Set(array)];
+
+export const binarySearch = (arr, l, r, x) => {
+    if(l > r) return -1;
+
+    const m = Math.floor((l + r) / 2);
+
+    if(arr[m] == x) return m;
+
+    if(arr[m] < x) {
+        return binarySearch(arr, m + 1, r, x);
+    } else {
+        return binarySearch(arr, l, m - 1, x);
+    }
+}
